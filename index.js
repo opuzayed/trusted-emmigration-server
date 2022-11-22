7d487d1f12edb0229c3c684246c90f612f3241f3
opuzayed <zayedopu4933@gmail.com>
2022-11-23 00:54:42 +0600

    db password and user setup done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 app.use(cors());
@@ -9,8 +10,14 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Emmigration server is running')
-})
+});
 
 app.listen(port, () => {
     console.log(`Emmigration  server is running on ${port}`);
-})
+});
+
+
+
+const uri = `mongodb+srv://${process.env.db-user}:${process.env.db-password}@cluster0.rsulfhn.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
