0192173591c7c2c36164b8c4610eda747367e496
opuzayed <zayedopu4933@gmail.com>
2022-11-24 00:39:30 +0600

    get all service data from mongodb

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
+const ObjectId = require('mongodb').ObjectId;
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -24,9 +25,18 @@ async function run() {
             res.send(visaServices);
         });
 
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const allServices = await cursor.toArray();
+            res.send(allServices);
+        });
+
+
+
         app.get('/service/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: ObjectId(id) };
+            const query = { _id : ObjectId(id) };
             const service = await serviceCollection.findOne(query);
             res.send(service);
         });
