587e58684bcf7b582972ba63247b72b08a5887af
opuzayed <zayedopu4933@gmail.com>
2022-11-23 21:15:49 +0600

    single data load from db

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
-const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rsulfhn.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try
+    {
+        const serviceCollection = client.db('emmigrationVisa').collection('services');
+
+        app.get('/home', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const visaServices = await cursor.limit(3).toArray();
+            res.send(visaServices);
+        });
+
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err));
 
 app.get('/', (req, res) => {
     res.send('Emmigration server is running')
@@ -15,9 +44,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Emmigration  server is running on ${port}`);
 });
-
-
-
-const uri = `mongodb+srv://${process.env.db-user}:${process.env.db-password}@cluster0.rsulfhn.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
