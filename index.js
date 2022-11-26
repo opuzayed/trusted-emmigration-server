461a1ed6819b97e2fdc43025a11d4306c44b907e
opuzayed <zayedopu4933@gmail.com>
2022-11-27 01:39:57 +0600

    data loaded to add service

--- index.js
+++ index.js
@@ -32,14 +32,64 @@ async function run() {
             res.send(allServices);
         });
 
-
-
         app.get('/service/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id : ObjectId(id) };
             const service = await serviceCollection.findOne(query);
             res.send(service);
         });
+
+        app.get('/addservice/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id : ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+
+        app.get('/addservice',async (req, res) => {
+            const decoded = req.decoded;
+            
+            if(decoded.email !== req.query.email){
+                res.status(403).send({message: 'unauthorized access'})
+            }
+
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        });
+
+        app.post('/addservice', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        });
+
+        app.patch('/addservice/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const status = req.body.status
+            const query = { _id: ObjectId(id) }
+            const updatedDoc = {
+                $set:{
+                    status: status
+                }
+            }
+            const result = await orderCollection.updateOne(query, updatedDoc);
+            res.send(result);
+        })
+
+        app.delete('/addservice/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            res.send(result);
+        })
+
     }
     finally {
 
